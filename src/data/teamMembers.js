e55f26c01221f4949bc27ed14ff5fc0008a60434
Asum Victor <iamasum369@gmail.com>
2024-06-06 11:05:40 +0000

    Update teamMembers.js

--- src/data/teamMembers.js
+++ src/data/teamMembers.js
@@ -6,19 +6,19 @@ import Fibo from "../images/Profile/fibo.jpeg";
 
 
 export const TeamMembers = [
-  {
-    image: Fibo,
-    role: "Linguistic researcher",
-    bio: `
-   I am determined to break down language barriers and enhance communication in healthcare delivery to people who have sensory and cognitive impairments. My goal is to gain a comprehensive understanding of AI technologies, algorithms, and language models through intensive studies and courses in computational linguistics and Natural Language Processing.
-By applying my knowledge in computational linguistics, I aspire to create innovative and intelligent technologies that can revolutionize healthcare services for those who are nonverbal, have hearing impairments, or suffer from language difficulties caused by brain disorders.
-    `,
-    name: "Asamoah Baffour Isaac ",
-    quote: `“The world is a beautiful mosaic of colors, cultures, and perspectives. Each piece is unique and valuable, and together we create a masterpiece. Let us appreciate and embrace the diversity of our world, and not let fear or ignorance divide us."`,
-    title: "Project Manager & linguistic researcher",
-    link: 'https://www.linkedin.com/in/morro-suleyman-067462263/'
+//  {
+//    image: Fibo,
+//    role: "Linguistic researcher",
+ //   bio: `
+//   I am determined to break down language barriers and enhance communication in healthcare delivery to people who have sensory and cognitive impairments. My goal is to gain a comprehensive understanding of AI technologies, algorithms, and language models through intensive studies and courses in computational linguistics and Natural Language Processing.
+// By applying my knowledge in computational linguistics, I aspire to create innovative and intelligent technologies that can revolutionize healthcare services for those who are nonverbal, have hearing impairments, or suffer from language difficulties caused by brain disorders.
+  //  `,
+ //   name: "Asamoah Baffour Isaac ",
+//    quote: `“The world is a beautiful mosaic of colors, cultures, and perspectives. Each piece is unique and valuable, and together we create a masterpiece. Let us appreciate and embrace the diversity of our world, and not let fear or ignorance divide us."`,
+//    title: "Project Manager & linguistic researcher",
+//    link: 'https://www.linkedin.com/in/morro-suleyman-067462263/'
     
-  },
+//  },
   {
     image: AsumVictor,
     role: "Programming Lead",
@@ -30,26 +30,26 @@ Again, I wish to be on TEDx as a public speaker.`,
     link: 'https://www.linkedin.com/in/victorasumdev/'
 
   },
-  {
-    image: Prince,
-    role: "Research Team Lead",
-    bio: `I am highly enthusiastic about pursuing an interdisciplinary advancement in Electrical Engineering and Computer Engineering, complemented by a sub-discipline in sociology. My passion lies in utilizing technology to tackle the problem of digital inequality that affects individuals with disabilities. Specifically, I aim to focus on integrating artificial intelligence and machine learning within the design of assistive technologies, with the ultimate goal of bridging the accessibility gap for these individuals while understanding the struggles faced by disabled persons through sociology. Here, I can combine my leadership, critical thinking, problem-solving skills, and understanding of social dynamics with my taste for electrical systems, computing infrastructure, and human assistive technologies to develop holistic approaches to tackle digital inequality in African societies.`,
-    name: "Amoah Prince Kofi Attah",
-    quote: `Concentrate on the abilities your disability doesn't hinder, and don't dwell on the things it interferes with. Be disabled physically, not in spirit. `,
-    title: "Researcher",
-    link: 'https://www.linkedin.com/in/victorasumdev/'
+//  {
+//    image: Prince,
+//    role: "Research Team Lead",
+//    bio: `I am highly enthusiastic about pursuing an interdisciplinary advancement in Electrical Engineering and Computer Engineering, complemented by a sub-discipline in sociology. My passion lies in utilizing technology to tackle the problem of digital inequality that affects individuals with disabilities. Specifically, I aim to focus on integrating artificial intelligence and machine learning within the design of assistive technologies, with the ultimate goal of bridging the accessibility gap for these individuals while understanding the struggles faced by disabled persons through sociology. Here, I can combine my leadership, critical thinking, problem-solving skills, and understanding of social dynamics with my taste for electrical systems, computing infrastructure, and human assistive technologies to develop holistic approaches to tackle digital inequality in African societies.`,
+//    name: "Amoah Prince Kofi Attah",
+//    quote: `Concentrate on the abilities your disability doesn't hinder, and don't dwell on the things it interferes with. Be disabled physically, not in spirit. `,
+//    title: "Researcher",
+//    link: 'https://www.linkedin.com/in/victorasumdev/'
     
-  },
-  {
-    image: Morro,
-    role: "Data Analyst",
-    bio: `Passionate about business and economics, I aim to excel in Business Administration while aspiring to become a top economist and successful entrepreneur. With a deep interest in analyzing market trends and economic policies, I aim to leverage this knowledge to drive positive change and create socially responsible businesses. Through practical experiences and academic pursuits, I am honing my skills to navigate the complexities of the business world and contribute to both economic growth and societal welfare.
+//  },
+//  {
+//    image: Morro,
+//    role: "Data Analyst",
+//    bio: `Passionate about business and economics, I aim to excel in Business Administration while aspiring to become a top economist and successful entrepreneur. With a deep interest in analyzing market trends and economic policies, I aim to leverage this knowledge to drive positive change and create socially responsible businesses. Through practical experiences and academic pursuits, I am honing my skills to navigate the complexities of the business world and contribute to both economic growth and societal welfare.
     
-    `,
-    name: "Suleyman Morro",
-    quote: `Empowering economies through insightful analysis, and businesses through innovative entrepreneurship, I strive to create a harmonious synergy between economic growth and societal well-being. `,
-    title: "Ashesi University",
-    link: 'https://www.linkedin.com/in/morro-suleyman-067462263/'
+  //  `,
+  //  name: "Suleyman Morro",
+ //   quote: `Empowering economies through insightful analysis, and businesses through innovative entrepreneurship, I strive to create a harmonious synergy between economic growth and societal well-being. `,
+//    title: "Ashesi University",
+  //  link: 'https://www.linkedin.com/in/morro-suleyman-067462263/'
     
-  },
+//  },
 ];
